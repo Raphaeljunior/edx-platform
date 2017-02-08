efd1684e64908f2ea069ecc55b5d3dad8a5f039a
Hasnain <hasnain.naveed123@gmail.com>
2017-02-08 15:07:49 +0500

    Program detail backend.

--- openedx/core/djangoapps/catalog/utils.py
+++ openedx/core/djangoapps/catalog/utils.py
@@ -25,11 +25,12 @@ def create_catalog_api_client(user, catalog_integration):
     return EdxRestApiClient(catalog_integration.internal_api_url, jwt=jwt)
 
 
-def get_programs(uuid=None, type=None):  # pylint: disable=redefined-builtin
+def get_programs(uuid=None, marketing_slug=None, type=None):  # pylint: disable=redefined-builtin
     """Retrieve marketable programs from the catalog service.
 
     Keyword Arguments:
         uuid (string): UUID identifying a specific program.
+        marketing_slug (string): Marketing slug indentifying a specific program.
         type (string): Filter programs by type (e.g., "MicroMasters" will only return MicroMasters programs).
 
     Returns:
@@ -45,8 +46,9 @@ def get_programs(uuid=None, type=None):  # pylint: disable=redefined-builtin
 
         api = create_catalog_api_client(user, catalog_integration)
 
-        cache_key = '{base}.programs{type}'.format(
+        cache_key = '{base}.programs{marketing_slug}{type}'.format(
             base=catalog_integration.CACHE_KEY,
+            marketing_slug='.' + marketing_slug if marketing_slug else '',
             type='.' + type if type else ''
         )
 
